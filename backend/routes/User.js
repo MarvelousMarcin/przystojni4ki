ad4d6a50ad45841c64397db4b4a967a7c42c3421
iamxdave <dawidwrobelx@gmail.com>
2022-11-19 19:50:04 +0100

    endpoints

--- backend/routes/User.js
+++ backend/routes/User.js
@@ -1,50 +1,84 @@
 const express = require("express");
+const bcrypt = require('bcrypt');
 const User = require("../models/UserSchema");
 const userRouter = express.Router();
 
-
-userRouter.get('/get/:id', async (req, res) => {
-    const user1 = new User ({
-        email: 'hello',
-        password: 'password'
-    });
-    
-    const user2 = new User ({
-        email: 'bye',
-        password: 'root'
-    });
-
-    //await user1.save();
-    await user2.save();
-    res.send(user1);
-});
-
 userRouter.get('/get', async (req, res) => {
-    const user1 = new User ({
-        email: 'hello',
-        password: 'password'
-    });
+    if(!req.body)
+        return res.status(400).send({ error: "Wrong body"});
     
-    const user2 = new User ({
-        email: 'bye',
-        password: 'root'
-    });
+    let { email } = req.body;
 
-    //await user1.save();
-    await user2.save();
-    res.send(user1);
+    const user = await User.findOne({ email }); 
+
+    if(!user)
+        return res.status(400).send({ error: "User not found"});
+    
+    res.send(user);
 });
 
-userRouter.put('/update', async (req, res) => {
-    res.send('UPDATE');
+userRouter.get('/getAll', async (req, res) => {
+    if(!User.find())
+        return res.status(400).send({ error: "Users not found"});
+    
+    res.send(await User.find());
 });
 
-userRouter.post('/create', async (req, res) => {
-    res.send('CREATE');
+userRouter.post('/register', async (req, res) => {
+    if(!req.body)
+        return res.status(400).send({ error: "Wrong body"});
+    
+    let { email, password} = req.body;
+
+    if(!email || !password)
+        return res.status(400).send({ error: "Wrong data"});
+    
+    const user = await User.findOne({ email }); 
+
+    console.log(user);
+    if(user)
+        return res.status(400).send({ error: "Email already used in database"});
+    
+    try {
+        password = bcrypt.hashSync(password, 10);
+        new User({ email, password }).save();
+        return 
+    } catch (error) {
+        return res.status(400).send({ error });
+    }
 });
 
-userRouter.delete('/delete', (req, res) => {
-    res.send('DELETE');
+userRouter.post('/login', async (req, res) => {
+    if(!req.body)
+        return res.status(400).send({ error: "Wrong body"});
+    
+    let { email, password} = req.body;
+
+    if(!email || password)
+        return res.status(400).send({ error: "Wrong data"});
+    
+    const user = await User.findOne({ email }); 
+
+    if(!user)
+        return res.status(400).send({ error: "Account with such email not found"});
+    
+    if(bcrypt.compare(user.password, password))
+        res.send({ correctPassowrd: true });
+    
+    res.send(403).send({ error: "Incorrect password"});
+    
+});
+
+userRouter.delete('/delete', async (req, res) => {
+    
+    const user = await User.findOne({ email }); 
+
+    if(!user)
+        return res.status(400).send({ error: "Account with such email not found"});
+    
+    User.deleteOne({ email });
+
+    return res.status(200).send();
 });
 
 module.exports = userRouter;
